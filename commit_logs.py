f7ac7f1251d9fc0bcda30eb580c29e5932f61a4b
VladGaranovskyi <garanovskiyvlad2005@gmail.com>
2023-12-01 18:02:04 -0400

    3DmAzWrUC5redW603G4QPdG3Q7m0MHNbiyUz5wBt8hBZZumlmX

--- commit_logs.py
+++ commit_logs.py
@@ -7,9 +7,8 @@ import string
 from datetime import date, datetime, timedelta
 
 # ---------- Config ----------
-START_DATE = date(2024, 12, 1)      # inclusive
+START_DATE = date(2023, 12, 1)      # inclusive
 TEXT_FILE = "notes.txt"             # file to touch
-REMOTE_NAME = "origin"              # remote name (usually 'origin')
 # ----------------------------
 
 def random_string(n=50):
@@ -24,7 +23,6 @@ def append_random_char(path):
     ch = random.choice(string.ascii_letters + string.digits)
     with open(path, "a", encoding="utf-8") as f:
         f.write(ch + "\n")
-    return ch
 
 def run_git(args, env=None):
     subprocess.run(["git", *args], check=True, env=env)
@@ -44,7 +42,6 @@ def main():
     subprocess.run(["git", "rev-parse", "--is-inside-work-tree"], check=True, stdout=subprocess.DEVNULL)
 
     ensure_file_exists(TEXT_FILE)
-    branch = "main"
     tz = datetime.now().astimezone().strftime("%z")
 
     today = date.today()
@@ -52,22 +49,25 @@ def main():
 
     with open("commit_ids.txt", "a", encoding="utf-8") as log:
         while d <= today:
-            commit_dt = datetime(d.year, d.month, d.day, random.randint(9, 23), random.randint(1, 50), random.randint(1, 50))
-            commit_date_iso = commit_dt.strftime(f"%Y-%m-%d %H:%M:%S {tz}")
+            n_commits = random.randint(0, 3)
+            
+            for _ in range(n_commits):
+                commit_dt = datetime(d.year, d.month, d.day, random.randint(9, 23), random.randint(1, 50), random.randint(1, 50))
+                commit_date_iso = commit_dt.strftime(f"%Y-%m-%d %H:%M:%S {tz}")
 
-            randch = append_random_char(TEXT_FILE)
-            randmsg = random_string(50)  # generate 50-char commit message
+                append_random_char(TEXT_FILE)
+                randmsg = random_string(50)  # generate 50-char commit message
 
-            commit_with_date(commit_date_iso, randmsg)
+                commit_with_date(commit_date_iso, randmsg)
 
-            cid = last_commit_hash()
-            log.write(f"{d.isoformat()} {cid} {randmsg}\n")
-            log.flush()
+                cid = last_commit_hash()
+                log.write(f"{d.isoformat()} {cid} {randmsg}\n")
+                log.flush()
 
-            run_git(["push"])
-            time.sleep(0.5)
+                run_git(["push"])
+                time.sleep(0.5)
 
-            d += timedelta(days=2)
+            d += timedelta(days=1)
 
 if __name__ == "__main__":
     main()
